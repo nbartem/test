0ca3b35fd139ec85f0a1704f3fe9944232bd8c45
Artem Liakh <liakh.artem@gmail.com>
2020-06-03 12:41:26 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import getRates from './getRates';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      base: '',
+      rates: [],
+      timestamp: '',
+      valid: true,
+    };
+  }
+  componentDidMount() {
+    getRates().then(data => {
+        this.setState(state => ({
+          base: data.base,
+          rates: data.rates,
+          timestamp: data.timestamp,
+          valid: data.valid,
+        }));
+      });
+  }
+  render() {
+    const rates = this.state.rates,
+      timestamp = this.state.timestamp,
+      base = this.state.base,
+      valid = this.state.valid;
+    let id = 0;
+    return (
+      <div>
+        <h1>{base}</h1>
+        <p>Timestamp: {timestamp}</p>
+        {valid ? null : <p>Information is not valid</p>}
+
+        <div className="flexbox">
+          <div>{Object.keys(rates).map(key => (
+            <p key={Object.keys(rates).indexOf(key)}>{key}</p>
+          ))}</div>          
+          <div>{Object.values(rates).map(value => {            
+            id++;
+            return <p key={id}>{value.toFixed(3)}</p>
+          })}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
